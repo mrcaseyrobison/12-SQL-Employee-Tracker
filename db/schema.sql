4979e5478a7922d8ec14eb1d4d94cee4aa97bfc1
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-09-02 18:53:04 -0700

    schema created

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,29 @@
+DROP DATABASE IF EXISTS theOffice_db;
+CREATE DATABASE theOffice_db;
+USE theOffice_db;
 
+-- DEPARTMENT TABLE --
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+-- ROLE TABLE --
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+-- EMPLOYEE TABLE --
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+)
